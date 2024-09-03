202106b55df7b6ee2e48998c6cd88d0bd144aa30
Iproduct Online Services <iproductonlineservices@gmail.com>
2024-09-03 18:56:13 +0530

    Added initial module file

---
+++ registration.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * IProduct Online Services
+ *
+ * Main regitration file
+ *
+ * This source file is subject to the IProduct Online Services.
+ * This is used to register the module
+ *
+ * PHP version 8.2
+ *
+ * @category  ProductKit
+ * @package   ProductKit
+ * @author    IProduct Online Services <iproduct@info.com>
+ * @copyright 2024 Iproduct
+ * @license   https://www.iproduct.in Iproduct
+ * @link      https://www.iproduct.in
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Iproduct_ProductKit',
+    __DIR__
+);
